ee68f40180ed5f881b0c6a6dec03502ff2d87b3f
James George <james@jamesgeorge.org>
2014-12-05 19:33:16 -0500

    port of a lot of code

--- src/ShapeDetector.cpp
+++ src/ShapeDetector.cpp
@@ -3,28 +3,172 @@
 //--------------------------------------------------------------
 void ShapeDetector::setup(){
 
+	//Initialize the sensors with a depth and color image channel
 	kinect.initSensor();
 	kinect.initDepthStream(true);
 	kinect.initColorStream();
 	kinect.start();
 
-	int width  = kinect.getDepthPixelsRef().getWidth(); 
-	int height = kinect.getDepthPixelsRef().getHeight();
+	depthImageWidth  = kinect.getDepthPixelsRef().getWidth(); 
+	depthImageHeight = kinect.getDepthPixelsRef().getHeight();
 
-	mappedColor.allocate(width,height, OF_IMAGE_COLOR);
+	depthColors.allocate(depthImageWidth,depthImageHeight,OF_IMAGE_COLOR);
+
+	background.allocate(		depthImageWidth, depthImageHeight , OF_IMAGE_GRAYSCALE);
+	thresholdedScene.allocate(	depthImageWidth, depthImageHeight , OF_IMAGE_GRAYSCALE);
+	scanScene.allocate(			depthImageWidth, depthImageHeight , OF_IMAGE_GRAYSCALE);
+	contourPix.allocate(		depthImageWidth, depthImageHeight , OF_IMAGE_GRAYSCALE);
+	depthColors.allocate(		depthImageWidth, depthImageHeight , OF_IMAGE_COLOR);
+
+	for(int i = 0; i < SHAPE_COLOR_COUNT; i++){
+		colorMasks[(ShapeColor)i].allocate(depthImageWidth, depthImageHeight, OF_IMAGE_GRAYSCALE);
+		maskedDepthColors[(ShapeColor)i].allocate(depthImageWidth, depthImageHeight, OF_IMAGE_COLOR);
+		colorDepthCompositeMask[(ShapeColor)i].allocate(depthImageWidth, depthImageHeight, OF_IMAGE_GRAYSCALE);
+	}
+
+	gui = new ofxUISuperCanvas("SHAPEGUI", 200,0,200,700);
+	gui->addRangeSlider("DEPTH RANGE",500,900,&minScanDistance,&maxScanDistance);
+	gui->addSlider("MIN AREA", 0, 100, &minArea);
+	gui->addSlider("SHIFT X", -10, 10, &shift.x);
+	gui->addSlider("SHIFT Y", -10, 10, &shift.y);
+	gui->addSpacer();
+	gui->addToggle("PREVIEW ELLIPSE",&previewEllipseFit);
+	gui->addToggle("PREVIEW RECT",&previewRectFit);
+	gui->addToggle("PREVIEW CIRCLE", &previewCircleFit);
+	gui->addToggle("PREVIEW STATS", &previewStats);
+
+	for(int i = 0; i < SHAPE_COLOR_COUNT; i++){
+		sampleConnectors[i] = false;
+	}
+
+	gui->loadSettings(getSettingsFilename());
+
+	loadColors();
 }
 
 //--------------------------------------------------------------
 void ShapeDetector::update(){
 	kinect.update();
 	if(kinect.isFrameNew()){
-		kinect.mapDepthToColor(mappedColor.getPixelsRef());
-		mappedColor.update();
+		kinect.mapDepthToColor(depthColors.getPixelsRef());
+		depthColors.update();
 	}
 }
 
 //--------------------------------------------------------------
 void ShapeDetector::draw(){
-	//kinect.draw(0,0);
-	mappedColor.draw(0,0);
+	depthColors.draw(0,0);
 }
+
+void ShapeDetector::createColorMasks(){
+	for(int i = 0; i < SHAPE_COLOR_COUNT; i++){
+		ShapeColor colorIndex = (ShapeColor)i;
+		createColorMask(colorIndex);
+	}	
+}
+
+void ShapeDetector::createColorMask(ShapeColor colorIndex){
+	
+	cv::Mat img = ofxCv::toCv(depthColors);
+	cv::Mat threshold = ofxCv::toCv(colorMasks[colorIndex]);
+	cv::Mat masked = ofxCv::toCv(maskedDepthColors[colorIndex]);
+
+	ofColor targetColor = colorSamples[colorIndex][0].color;
+	cv::Scalar offset(colorSamples[colorIndex][0].hueRange, 
+					  colorSamples[colorIndex][0].saturationRange, 
+					  colorSamples[colorIndex][0].valueRange);
+
+	cv::cvtColor(img, hsvImage, CV_RGB2HSV);
+	cv::Scalar base = ofxCv::toCv(ofxCv::convertColor(targetColor, CV_RGB2HSV));
+	cv::Scalar lowerb = base - offset;
+	cv::Scalar upperb = base + offset;
+	cv::inRange(hsvImage, lowerb, upperb, tempThresh);
+	
+	masked.setTo(0);
+	img.copyTo(masked,tempThresh);
+
+	targetColor = colorSamples[colorIndex][1].color;
+	offset = cv::Scalar(colorSamples[colorIndex][1].hueRange, 
+						colorSamples[colorIndex][1].saturationRange, 
+					    colorSamples[colorIndex][1].valueRange);
+
+	cv::cvtColor(img, hsvImage, CV_RGB2HSV);
+	base = ofxCv::toCv(ofxCv::convertColor(targetColor, CV_RGB2HSV));
+	lowerb = base - offset;
+	upperb = base + offset;
+	cv::inRange(hsvImage, lowerb, upperb, threshold);
+
+	img.copyTo(masked,threshold);
+	cv::add( threshold, tempThresh, threshold );
+
+	colorMasks[colorIndex].update();
+	maskedDepthColors[colorIndex].update();
+	
+}
+
+void ShapeDetector::exit(){
+	saveColors();
+	gui->saveSettings(getSettingsFilename());
+}
+
+
+void ShapeDetector::saveColors(){
+	ofBuffer colorBuffer;
+
+	for(int s = 0; s < 2; s++){
+		for(int i = 0; i < SHAPE_COLOR_COUNT; i++){
+			colorBuffer.append( ofToString( (int) colorSamples[(ShapeColor)i][s].color.r) + "," +
+								ofToString( (int) colorSamples[(ShapeColor)i][s].color.g) + "," +
+								ofToString( (int) colorSamples[(ShapeColor)i][s].color.b) + "," +
+								ofToString( (int) colorSamples[(ShapeColor)i][s].hueRange) + "," +
+								ofToString( (int) colorSamples[(ShapeColor)i][s].saturationRange) + "," +
+								ofToString( (int) colorSamples[(ShapeColor)i][s].valueRange) + "\n");
+		}
+	}
+	ofBufferToFile(getColorFilename(),colorBuffer);
+}
+
+void ShapeDetector::loadColors(){
+
+	if(!ofFile(getColorFilename()).exists()){
+		return;
+	}
+
+	ofBuffer colorBuffer = ofBufferFromFile(getColorFilename());
+	int index = 0;
+	while(!colorBuffer.isLastLine()){
+		string line = colorBuffer.getNextLine();
+		vector<string> colorComponents = ofSplitString(line,",",true,true);
+		ShapeColor i = (ShapeColor )(index % SHAPE_COLOR_COUNT);
+		int s = index/SHAPE_COLOR_COUNT;
+		colorSamples[i][s].color = 
+			ofColor(ofToInt(colorComponents[0]),
+					ofToInt(colorComponents[1]),
+					ofToInt(colorComponents[2]));
+
+		if(colorComponents.size() > 3){
+			colorSamples[i][s].hueRange			= ofToInt(colorComponents[3]);
+			colorSamples[i][s].saturationRange	= ofToInt(colorComponents[4]);
+			colorSamples[i][s].valueRange		= ofToInt(colorComponents[5]);
+		}
+		else{
+			colorSamples[i][s].hueRange			= 0;
+			colorSamples[i][s].saturationRange	= 0;
+			colorSamples[i][s].valueRange		= 0;
+		}
+		index++;
+	}
+}
+
+string ShapeDetector::getSettingsFilename(){
+	return "settings/settings.xml";
+}
+
+string ShapeDetector::getColorFilename(){
+	return "settings/colors.xml";	
+}
+
+string ShapeDetector::getColorRangeFilename(){
+	return "settings/colorranges.xml";	
+}
+
--- src/ShapeDetector.h
+++ src/ShapeDetector.h
@@ -3,6 +3,69 @@
 #include "ofMain.h"
 #include "ShapeDetector.h"
 #include "ofxKinectCommonBridge.h"
+#include "ofxCv.h"
+#include "ofxUI.h"
+
+struct CurioContour {
+	
+	//FROM SCAN
+	float depthRangeMin;
+	float depthRangeMax;
+	ofPolyline contour;
+	
+	//FROM CV
+	bool valid;
+	cv::Rect boundingRect;
+	cv::RotatedRect fitEllipse;
+	cv::RotatedRect fitRect;
+	
+	ofVec2f circlePosition;
+	float circleRadius;
+	float rectMaxSide;
+	
+	//COMPUTED
+	float depthPosition; //depth of sensed position
+	float coordRadius; //radius of sense cross section
+};
+
+enum ShapeColor {
+
+	COLOR_SPHERE_LIGHTGREEN = 0,
+	COLOR_SPHERE_BLUE,
+	COLOR_SPHERE_MAROON,
+		
+	COLOR_CUBE_LAVENDER,
+	COLOR_CUBE_RED,
+	COLOR_CUBE_SALMON,
+
+	COLOR_CONNECTOR_CYAN,
+	COLOR_CONNECTOR_GREEN,
+	COLOR_CONNECTOR_PURPLE,
+	COLOR_CONNECTOR_ORANGE,
+	COLOR_CONNECTOR_FUSCHIA,
+	COLOR_CONNECTOR_YELLOW,
+
+	SHAPE_COLOR_COUNT
+};
+
+
+//color selection and layer tracking
+//-- add color picker
+//-- add color layers 
+//-- add debug view
+//-- add 3D view
+//-- make kinect mapping faster
+
+struct CurioColorSlider {
+	ofRectangle samplePos;
+	ofRectangle hpos;
+	ofRectangle spos;
+	ofRectangle vpos;
+	ofColor color;
+	int hueRange;
+	int saturationRange;
+	int valueRange;
+};
 
 class ShapeDetector
 {
@@ -10,7 +73,53 @@ class ShapeDetector
 	void setup();
 	void update();
 	void draw();
+	void exit();
 
+
+  protected:
 	ofxKinectCommonBridge kinect;
-	ofImage mappedColor;
+	int depthImageWidth;
+	int depthImageHeight;
+
+	ofxUISuperCanvas* gui;
+	//scanning parameters
+	ofVec2f shift;
+	float minScanDistance;
+	float maxScanDistance;
+	float minArea;
+
+	//visualization parameters
+	bool showAllContours;
+	bool previewColors;
+	bool previewEllipseFit;
+	bool previewRectFit;
+	bool previewCircleFit;
+	bool previewStats;
+
+	bool sampleConnectors[SHAPE_COLOR_COUNT];
+	int sampleColorIndex;
+
+	void saveColors();
+	void loadColors();
+
+	void createColorMask(ShapeColor colorIndex);
+	void createColorMasks();
+
+	ofImage depthColors;
+	ofShortImage background;
+	ofShortImage scanScene;
+	ofShortImage thresholdedScene;
+	ofPixels contourPix;
+
+	map<ShapeColor, vector<CurioColorSlider> > colorSamples;
+	map<ShapeColor, ofImage> colorMasks;
+	map<ShapeColor, ofImage> maskedDepthColors;
+	map<ShapeColor, ofFloatImage> colorDepthCompositeMask;
+
+	string getSettingsFilename();
+	string getColorFilename();
+	string getColorRangeFilename();
+
+	cv::Mat hsvImage;
+	cv::Mat tempThresh;
 };
